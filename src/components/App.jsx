b6c934cb966c959546945d6f61a07c366122523a
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-30 18:34:55 -0700

    Import future modules for display and live testing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import Header from './Header/Header.jsx';
 import Body from './Body.jsx';
 
 
 function App(){
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={Body}/>
       </Switch>
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Keg from './Keg/Keg.jsx';
+import Main from './Main/Main.jsx';
+import Edit from './Edit/Edit.jsx';
 
 function Body(){
   return (
     <div>
       <p>HELLO!!!</p>
+      <Keg/>
+      <Main/>
+      <Edit/>
     </div>
   );
 }
