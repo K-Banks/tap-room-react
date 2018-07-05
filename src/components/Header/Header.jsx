37e2fa2c77b198a65b00a2eaab7f065536a7ad45
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-05 14:42:58 -0700

    Update header of application, adding sample data to Keg component for temporary testing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import styles from './Header.css';
 function Header(){
   return (
     <div>
-      <p>Header!!!</p>
+      <h1>Welcome to Ghibli Taps</h1>
+      <h2>Offering up imaginary brews for you</h2>
+      <img src='https://static1.srcdn.com/wordpress/wp-content/uploads/spirited-away-cover.jpg' alt='Ghibli image' className={styles.banner}/>
     </div>
   );
 }
--- src/components/Keg/Keg.jsx
+++ src/components/Keg/Keg.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './Keg.css';
 
+const keg = {
+  name: 'Ghibli Brew',
+  brand: 'Porco Rosso',
+  price: '5.00',
+  abv: '5.6',
+  remaining: 12,
+  description: 'an imaginitive brew just for you'
+}
+
 function Keg(){
   return (
     <div>
-      <p>Keg!!!</p>
+      <h3>{keg.name}</h3>
+      <h3>{keg.brand}</h3>
+      <h3>{keg.price}</h3>
+      <h3>{keg.abv}</h3>
+      <h3>{keg.remaining}</h3>
+      <h3>{keg.description}</h3>
     </div>
   );
 }
