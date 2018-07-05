203906c7f7c629f10c3dfba1d15f9381af43e82c
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-05 16:08:12 -0700

    Keeping Favicon component but no longer trying to implement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Header from './Header/Header.jsx';
 import UserBody from './UserBody.jsx';
-import Favicon from './Favicon.jsx';
 
 function App(){
   return (
     <div>
-      <Favicon/>
       <Header/>
       <Switch>
         <Route exact path='/' component={UserBody}/>
