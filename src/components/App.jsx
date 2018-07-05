d079cefa6ce5d9d92c3c047833749932e042c27e
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-05 16:06:35 -0700

    Experimenting with implementing react-favicon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Header from './Header/Header.jsx';
 import UserBody from './UserBody.jsx';
-
+import Favicon from './Favicon.jsx';
 
 function App(){
   return (
     <div>
+      <Favicon/>
       <Header/>
       <Switch>
         <Route exact path='/' component={UserBody}/>
---
+++ src/components/Favicon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Favicon from 'react-favicon';
+
+ReactDOM.render(
+  <div>
+    <Favicon url='./assets/images/favicon.ico'/>
+  </div>
+  , document.getElementById('root'));
+
+export default Favicon;
