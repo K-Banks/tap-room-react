3fc60b5b064baad3fc13610d90659ada282f1e9a
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-06 10:02:07 -0700

    Attempting to pass state down to KegList, currently returning error on compile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,61 @@ import Footer from './Footer/Footer.jsx';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterKegList: {
+        1: {
+          name: 'The Red Pig Amber Ale',
+          brand: 'Porco Rosso',
+          price: 7.00,
+          abv: 5.6,
+          remaining: 9,
+          description: 'An ale straight from the Adriatic'
+        },
+        2: {
+          name: 'Sky Pirate Lager',
+          brand: 'Laputa',
+          price: 4.00,
+          abv: 6.8,
+          remaining: 110,
+          description: 'Talk like a (sky)pirate'
+        },
+        3: {
+          name: 'Howl and Growl',
+          brand: 'Howl\'s Moving Castle',
+          price: 6.50,
+          abv: 4.8,
+          remaining: 98,
+          description: 'Whine like a true wizard'
+        },
+        4: {
+          name: 'Ashitaka Ale',
+          brand: 'Princess Mononoke',
+          price: 7.50,
+          abv: 8.5,
+          remaining: 13,
+          description: 'Brewed by the King of the Forest himself'
+        },
+        5: {
+        name: 'Witch\'s Brew',
+        brand: 'Kiki\'s Delivery Service',
+        price: 4.50,
+        abv: 5.0,
+        remaining: 57,
+        description: 'Something clever about a witch and a cauldron'
+      },
+      }
+    };
+  }
+
   render(){
     return (
       <div className={styles.gradient}>
         <div className={styles.wrapper}>
           <Header/>
           <Switch>
-            <Route exact path='/' component={Keglist}/>
+            <Route exact path='/' render={()=><KegList kegList={this.state.masterKegList}/>}/>
             <Route component={Error404}/>
           </Switch>
           <Footer/>
--- src/components/KegList/KegList.jsx
+++ src/components/KegList/KegList.jsx
@@ -2,64 +2,31 @@ import React from 'react';
 import styles from './KegList.css';
 import Keg from '../Keg/Keg.jsx';
 import NewKegForm from '../NewKegForm/NewKegForm.jsx';
+import PropTypes from 'prop-types';
 
-const masterKegList = [
-  {
-    name: 'The Red Pig Amber Ale',
-    brand: 'Porco Rosso',
-    price: 7.00,
-    abv: 5.6,
-    remaining: 9,
-    description: 'An ale straight from the Adriatic'
-  },{
-    name: 'Sky Pirate Lager',
-    brand: 'Laputa',
-    price: 4.00,
-    abv: 6.8,
-    remaining: 110,
-    description: 'Talk like a (sky)pirate'
-  },{
-    name: 'Howl and Growl',
-    brand: 'Howl\'s Moving Castle',
-    price: 6.50,
-    abv: 4.8,
-    remaining: 98,
-    description: 'Whine like a true wizard'
-  },{
-    name: 'Ashitaka Ale',
-    brand: 'Princess Mononoke',
-    price: 7.50,
-    abv: 8.5,
-    remaining: 13,
-    description: 'Brewed by the King of the Forest himself'
-  },{
-    name: 'Witch\'s Brew',
-    brand: 'Kiki\'s Delivery Service',
-    price: 4.50,
-    abv: 5.0,
-    remaining: 57,
-    description: 'Something clever about a witch and a cauldron'
-  }
-];
-
-function KegList(){
+function KegList(props){
   return (
     <div>
       <h2>Beers On Tap:</h2>
       <div className={styles.flex}>
-        {masterKegList.map((keg, index) =>
-            <Keg name={keg.name}
-            brand={keg.brand}
-            price={keg.price}
-            abv={keg.abv}
-            remaining={keg.remaining}
-            description={keg.description}
-            key={index}/>
-        )}
+        {Object.keys(props.kegList).map(function(kegId) {
+          var keg = props.kegList[kegId];
+          return <Keg name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          abv={keg.abv}
+          remaining={keg.remaining}
+          description={keg.description}
+          key={keg}/>;
+        })}
       </div>
       <NewKegForm/>
     </div>
   );
 }
+
+KegList.propTypes = {
+  kegList: PropTypes.object
+};
 
 export default KegList;
