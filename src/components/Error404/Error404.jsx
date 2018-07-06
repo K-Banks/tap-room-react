8b305cfb542fd8f116141c2cd2b584e3edca8bb1
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-06 16:05:36 -0700

    Edit styling

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -7,7 +7,7 @@ function Error404(props) {
   return(
     <div>
       <h3>This page, {props.location.pathname}, does not exist.</h3>
-      <h4><Link to='/'>Click here to go back to the homepage</Link></h4>
+      <h4><Link className={styles.link} to='/'>Click here to go back to the homepage</Link></h4>
       <img src='http://junkiemonkeys.com/wp-content/uploads/2014/08/RIPStudioGhibli.png' alt='Error 404: Page not found'/>
     </div>
   );
